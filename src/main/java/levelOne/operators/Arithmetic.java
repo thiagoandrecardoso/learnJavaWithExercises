7063eb1fe4fb5edef80167e71d0fb168c6980f5f
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-04-05 00:25:34 -0300

    Level one testing started

---
+++ src/main/java/levelOne/operators/Arithmetic.java
@@ -0,0 +1,63 @@
+package levelOne.operators;
+
+/**
+ * Os testes para estes metodos estao em:
+ * src.test.java.levelOne.operators
+ * MODIFIQUE OS RETURNS
+ */
+public class Arithmetic {
+
+    public int addition(int a, int b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    public int subtraction(int a, int b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    /**
+     * Retornar o valor 0 quando o divisor for 0
+     */
+    public int division(int a, int b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    public int multiplication(int a, int b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    public int restOfDivision(int a, int b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    /**
+     * Daqui seguimos com funcoes com um pequeno grau de dificuldade a mais
+     */
+
+    // Esta funcao deve retornar o dobro de a somado a b
+    public double calculationOne(double a, double b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    // Esta funcao deve retornar o dobro de a somado ao dobro de b
+    public double calculationTwo(double a, double b) {
+        // implemente a função aqui.
+        return -1;
+    }
+
+    /**
+     * Implemente o calculo da conversao de graus Celcius para Fahrenheit
+     */
+    public double celsiusToFahrenheit(double celsius) {
+        // implemente a função aqui.
+        return -1.0;
+    }
+
+
+}
---
+++ src/test/java/levelOne/operators/ArithmeticTest.java
@@ -0,0 +1,109 @@
+package levelOne.operators;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArithmeticTest {
+    /**
+     * Voce pode clicar encima do nome do metodo e com o botao direito
+     * rodar ele separadamente
+     * Ou pode clicar abaixo do cabecalho da classe e rodar todos os testes
+     */
+
+    Arithmetic arithmetic;
+
+    @Before
+    public void setUp() throws Exception {
+        arithmetic = new Arithmetic();
+    }
+
+    /**
+     * testes para formulas de adicao e subtraction:
+     */
+    @Test
+    public void addition1() {
+        assertEquals(10, arithmetic.addition(3, 7));
+    }
+
+    @Test
+    public void addition2() {
+        assertEquals(0, arithmetic.addition(3, -3));
+    }
+
+    @Test
+    public void subtraction1() {
+        assertEquals(6, arithmetic.subtraction(3, -3));
+    }
+
+    @Test
+    public void subtraction2() {
+        assertEquals(300, arithmetic.subtraction(300, 0));
+    }
+
+    /**
+     * testes para formulas de multiplicacao e divisao:
+     */
+    @Test
+    public void division1() {
+        assertEquals(3, arithmetic.division(30, 10));
+    }
+
+    @Test
+    public void division2() {
+        assertEquals(0, arithmetic.division(2, 0));
+    }
+
+    @Test
+    public void multiplication1() {
+        assertEquals(0, arithmetic.multiplication(2, 0));
+    }
+
+    @Test
+    public void multiplication2() {
+        assertEquals(-24, arithmetic.multiplication(12, -2));
+    }
+
+    /**
+     * Outras funcoes
+     */
+    @Test
+    public void calculationOne() {
+        assertEquals(-22, arithmetic.calculationOne(12, -2));
+    }
+
+    @Test
+    public void calculationTwo() {
+        assertEquals(-20, arithmetic.calculationTwo(12, -2));
+    }
+
+    @Test
+    public void calculationOne1() {
+        assertEquals(-2, arithmetic.calculationOne(0, -2));
+    }
+
+    @Test
+    public void calculationTwo1() {
+        assertEquals(400, arithmetic.calculationTwo(100, 100));
+    }
+
+    /**
+     * Teste para conversao de graus Celcius para Fahrenheit
+     */
+    @Test
+    public void celsiusToFahrenheit1() {
+        assertEquals(32, arithmetic.celsiusToFahrenheit(0));
+    }
+
+    @Test
+    public void celsiusToFahrenheit2() {
+        assertEquals(89.6, arithmetic.celsiusToFahrenheit(32));
+    }
+
+    @Test
+    public void celsiusToFahrenheit3() {
+        assertEquals(30.2, arithmetic.celsiusToFahrenheit(-1));
+    }
+
+}
